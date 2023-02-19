aa87a9fdc5f977c9afb81026b7a4275258df9b1f
ika <287720499@qq.com>
2023-02-19 12:39:16 +0800

    Initial commit

---
+++ evaluate_bird.py
@@ -0,0 +1,49 @@
+from flappy import FlappyBird
+import numpy as np
+import matplotlib.pyplot as plt
+from algorithms import q_learning
+
+def evaluate_learning(
+        sim,series_size=100, num_series=40, gamma=0.99, alpha=0.7,
+        epsilon=0.001):
+    '''
+    :param sim: The queuing object
+    :param series_size: the size of series
+    :param num_series: the number of series
+    :return: a result policy
+    '''
+
+    # initialise Q values
+
+    Q = 0*np.ones((sim.num_states,sim.num_actions))
+    #Q = np.load('save.npy')
+    figrew, axrew = plt.subplots()
+    total_reward_seq = [0]
+    total_episodes = 0
+
+    for series in range(num_series):
+        print("series = %r/%d" % (series,num_series) ) # Print the current stage
+        rewardlist=[]
+        for episode in range(series_size):
+            Q = q_learning(
+                sim, gamma=gamma, alpha=alpha, epsilon=epsilon,
+                num_episodes=1, Qtable = Q)
+            rewardlist.append(
+                sim.score)
+            total_episodes += 1
+        Q2=Q
+        total_reward_seq.append(np.mean(np.array(rewardlist)))
+
+        np.save('save.npy',Q2)
+    total_reward_seq = np.array(total_reward_seq)
+    axrew.plot(
+        np.arange(0, total_episodes+1, series_size),
+        total_reward_seq)
+    axrew.set_xlabel("episodes")
+    axrew.set_ylabel("average score")
+    return
+
+evaluate_learning(sim=FlappyBird())
+plt.title('Average score per series')
+plt.tight_layout()
+plt.show()
